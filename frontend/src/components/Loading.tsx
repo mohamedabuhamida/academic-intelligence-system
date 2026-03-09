bb51eb5b740673e109b92a42616942a8767e5b06
Abuhamida <mr6303302@gmail.com>
2026-03-09 07:43:50 +0200

    Updates: 2026-03-09T05:43:48.260Z

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+import { SpinnerInfinity } from 'spinners-react'
+
+export default function Loading() {
+  return (
+    <div className='flex flex-col justify-center items-center w-full'>
+        <SpinnerInfinity/>
+    </div>
+  )
+}
